2787024160fba413f57ba47fd13bac8ec32e3d59
gandzikbeep <gandzikbeep@gmail.com>
2020-09-16 08:39:51 +0200

    test

--- src/main/java/BaseConfig/BaseTest.java
+++ src/main/java/BaseConfig/BaseTest.java
@@ -19,7 +19,7 @@ public class BaseTest {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-        driver.get(baseUrl); //t
+        driver.get(baseUrl); //t6y6yh6y6
     }
 
     @After
