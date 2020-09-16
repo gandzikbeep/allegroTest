c67ac9597e13d0301afb25c6273cf4fe99aa6e5b
gandzikbeep <gandzikbeep@gmail.com>
2020-09-16 08:38:12 +0200

    new project allegro(basepage, basetest, HomePage)v

--- src/main/java/BaseConfig/BaseTest.java
+++ src/main/java/BaseConfig/BaseTest.java
@@ -19,7 +19,7 @@ public class BaseTest {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-        driver.get(baseUrl);
+        driver.get(baseUrl); //t
     }
 
     @After
